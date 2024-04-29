273afa2e7bb05e7812096a47a822a7df1985b912
miloszbrzezinski <109414155+miloszbrzezinski@users.noreply.github.com>
2024-04-29 11:01:34 +0200

    openCV library added, and returning raw web cam image

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,26 @@
 from fastapi import FastAPI
+from fastapi.responses import StreamingResponse
+
+import cv2 as cv
 
 
 app = FastAPI()
 
 
-@app.get("/api")
+def get_raw_image():
+    camera = cv.VideoCapture(0)
+
+    while True:
+        success, frame = camera.read()
+        if success:
+            buffer = cv.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        else:
+            camera.release()
+
+
+@app.get("/raw")
 def read_root():
-    return {"message": "success"}
+    return StreamingResponse(get_raw_image(), media_type="multipart/x-mixed-replace;boundary=frame")
